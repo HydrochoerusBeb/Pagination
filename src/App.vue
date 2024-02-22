19de9ba15b1b1d22b6e57d09ec5ec7d0b9306980
HydrochoerusBeb <ap139802@gmai.com>
2024-02-22 20:40:18 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template lang="">
+  <div class='for-border container-width'>
+    <h1>Recipes</h1>
+   <Pag></Pag>
+  </div>
+</template>
+<script>
+import Pag from './components/Pag.vue';
+export default {
+  components:{
+    Pag
+  }
+}
+</script>
+<style>
+  body{
+    font-family: 'Inter', sans-serif;
+  }
+  h1{
+    font-weight: 400px;
+    font-size: 48px;
+    
+  }
+  .container-width{
+    width: 774px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="button-container">
+      <button @click="changePage(currentPage - 1)" :disabled="currentPage === 1"><</button>
+      <button v-for="page in totalPages" :key="page" @click="changePage(page)" :disabled="currentPage === page">{{ page }}</button>
+      <button @click="changePage(currentPage + 1)" :disabled="currentPage === totalPages">></button>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    props: {
+      totalPages: {
+        type: Number,
+        required: true
+      },
+      currentPage: {
+        type: Number,
+        required: true
+      }
+    },
+    setup(props, { emit }) {
+      const changePage = (pageNumber) => {
+        if (pageNumber >= 1 && pageNumber <= props.totalPages) {
+          emit('pageChanged', pageNumber);
+        }
+      };
+  
+      return {
+        changePage
+      };
+    }
+  };
+  </script>
+
+<style>
+    .button-container{
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+    }
+
+    .button-container button{ 
+        background-color: transparent;
+        width: 64px;
+        height: 64px;
+        border: 1px solid #999999; 
+        border-radius: 5px; 
+        color: #999999; 
+        padding: 20px;
+        cursor: pointer;
+        transition: background-color 0.3s ease; 
+    }
+
+    .button-container button:hover  {
+    background-color: #007bff;
+    color: #ffffff;
+    }
+</style>
+    
